25a02cfcff1d4e57cda788dbb5364692bcf5ceeb
Dale Stone <dale@stonenet.co.uk>
2018-12-08 19:59:32 +0000

    refactor: renamed class and added console project for examples

---
+++ SecretSharing/Interpolation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSharing
+{
+    public class Interpolation
+    {
+        /// <summary>
+        /// Return the unique degree n polynomial passing through the given n+1 points.
+        /// </summary>
+        public void Interpolate()
+        {
+
+        }
+
+        /// <summary>
+        /// Return one term of an interpolated polynomial.
+        /// </summary>
+        public void SingleTerm()
+        {
+
+        }
+    }
+}
