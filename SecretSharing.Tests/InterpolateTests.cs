100be96f1713c72980764bfc0a247d15dc4fe9c7
Dale Stone <dale@stonenet.co.uk>
2018-12-08 19:50:50 +0000

    test: added placeholder interpolate tests

---
+++ SecretSharing.Tests/InterpolateTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SecretSharing.Tests
+{
+    public class InterpolateTests
+    {
+        [Fact]
+        public void Test_Interpolate_Empty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Test_Interpolate_Repeated_X_Values()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Test_Interpolate_Degree_0()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Test_Interpolate_Degree_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Test_Interpolate_Degree_3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SecretSharing/Interpolate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSharing
+{
+    public class Interpolate
+    {
+        public Interpolate()
+        {
+
+        }
+
+        public void SingleTerm()
+        {
+
+        }
+    }
+}
